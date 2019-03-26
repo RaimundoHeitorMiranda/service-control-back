2e426b7beb99e729b5d09a4668d3da1324eef4e4
RaimundoHeitorMiranda <raimundo.miranda@ccc.ufcg.edu.br>
2019-03-26 14:41:06 -0300

    Fisrt commit

---
+++ src/main/java/com/servicecontrol/dev/ServiceControlBackApplication.java
@@ -0,0 +1,13 @@
+package com.servicecontrol.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceControlBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceControlBackApplication.class, args);
+	}
+
+}
